f56914f8f5e1f504c87ecfb6a55ca479374c533b
round00 <zzzero520@gmail.com>
2018-11-06 19:11:47 +0800

    添加tab页面切换，添加背景图片

--- index.js
+++ index.js
@@ -2,5 +2,9 @@ $(function () {
     $('.tabs  li').on('click',function (e) {
         $(this).siblings().removeClass('active');
         $(this).addClass('active');
+        var index = $(this).index();
+        // 切换tab页面
+        $('.tabContent>ul').children().eq(index).addClass('active').siblings().removeClass('active');
+
     })
 })
