569d2306e14ac0fe536e97acde23c60fa51ba444
round00 <zzzero520@gmail.com>
2018-11-07 11:01:39 +0800

    添加loading 更改字体大小

--- index.js
+++ index.js
@@ -7,4 +7,18 @@ $(function () {
         $('.tabContent>ul').children().eq(index).addClass('active').siblings().removeClass('active');
 
     })
+
+    // 获取最新音乐列表
+    $.get('./latest_music.json').done(function (result) {
+        result.map(function (res,index) {
+            var $li = $(".latestMusic>ul").children().eq(index);
+            console.log($li);
+            $li.find('h4').text(res['name']);
+            var s = res['singer'] + '-' + res['album'];
+            $li.find('p').text(s);
+        })
+
+        $('.loading').remove();
+        $('.latestMusic').addClass('active');
+    })
 })
