27a0baab433ac800f2c157c9a720865cf02886ec
round00 <zzzero520@gmail.com>
2018-11-07 14:55:18 +0800

    完成热歌榜tab页

--- index.js
+++ index.js
@@ -1,24 +1,58 @@
 $(function () {
+
+    loadLatestMusic();
+
     $('.tabs  li').on('click',function (e) {
         $(this).siblings().removeClass('active');
         $(this).addClass('active');
         var index = $(this).index();
+
         // 切换tab页面
         $('.tabContent>ul').children().eq(index).addClass('active').siblings().removeClass('active');
+
+        if(index === 1){
+            loadHotMusic();
+        }
 
     })
 
     // 获取最新音乐列表
-    $.get('./latest_music.json').done(function (result) {
-        result.map(function (res,index) {
-            var $li = $(".latestMusic>ul").children().eq(index);
-            console.log($li);
-            $li.find('h4').text(res['name']);
-            var s = res['singer'] + '-' + res['album'];
-            $li.find('p').text(s);
-        })
+    function loadLatestMusic() {
+        var $ul = $(".playList .latestMusic>ul");
+        if($ul.attr('dataLoaded') === 'yes'){ return;}
 
-        $('.loading').remove();
-        $('.latestMusic').addClass('active');
-    })
+        $.get('./latest_music.json').done(function (result) {
+            result.map(function (res,index) {
+                var $li = $ul.children().eq(index);
+                $li.find('h4').text(res['name']);
+                var s = res['singer'] + '-' + res['album'];
+                $li.find('p').text(s);
+            })
+
+            $('.playList .loading').remove();
+            $('.playList .latestMusic').addClass('active');
+            $ul.attr('dataLoaded','yes');
+        })
+    }
+    // 获取热歌榜
+    function loadHotMusic() {
+        var $ul = $('.hotList .musicList');
+        if($ul.attr('dataLoaded') === 'yes'){ return; }
+
+        $.get('./hot_music.json').done(function (result) {
+            result.map(function (value,index) {
+                var $li = $ul.children().eq(index);
+                $li.find('h4').text(value['name']);
+                var s = value['singer'] + '-' + value['album'];
+                $li.find('p').text(s);
+            })
+            $ul.attr('dataLoaded','yes');
+            $('.hotList .loading').remove();
+            $('.hotList .musicList').addClass('active');
+
+        })
+    }
+
+
+
 })
